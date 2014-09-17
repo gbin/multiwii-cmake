a376f13cd266dafb6e44bf1c7b6664dbbc540323
Guillaume Binet <gbin@gootz.net>
2014-09-17 08:51:40 -0700

    removed redefinition warnings

--- src/config.h
+++ src/config.h
@@ -72,7 +72,7 @@
     #define MINCOMMAND  1000
 
   /**********************************  I2C speed for old WMP config (useless config for other sensors)  *************/
-    #define I2C_SPEED 100000L     //100kHz normal mode, this value must be used for a genuine WMP
+    //#define I2C_SPEED 100000L     //100kHz normal mode, this value must be used for a genuine WMP
     //#define I2C_SPEED 400000L   //400kHz fast mode, it works only with some WMP clones
 
   /***************************    Internal i2c Pullups   ********************************/
--- src/def.h
+++ src/def.h
@@ -592,16 +592,17 @@
   #define RX_PC_INTERRUPT            PCINT2_vect
   #define RX_PCINT_PIN_PORT          PINK
   
-  #define SERVO_1_PINMODE            pinMode(34,OUTPUT);pinMode(44,OUTPUT); // TILT_PITCH - WING left
-  #define SERVO_1_PIN_HIGH           PORTC |= 1<<3;PORTL |= 1<<5;
-  #define SERVO_1_PIN_LOW            PORTC &= ~(1<<3);PORTL &= ~(1<<5);
-  #define SERVO_2_PINMODE            pinMode(35,OUTPUT);pinMode(45,OUTPUT); // TILT_ROLL  - WING right
-  #define SERVO_2_PIN_HIGH           PORTC |= 1<<2;PORTL |= 1<<4;
-  #define SERVO_2_PIN_LOW            PORTC &= ~(1<<2);PORTL &= ~(1<<4);
-  #define SERVO_3_PINMODE            pinMode(33,OUTPUT); pinMode(46,OUTPUT); // CAM TRIG  - alt TILT_PITCH
-  #define SERVO_3_PIN_HIGH           PORTC |= 1<<4;PORTL |= 1<<3;
-  #define SERVO_3_PIN_LOW            PORTC &= ~(1<<4);PORTL &= ~(1<<3);
-  #define SERVO_4_PINMODE            pinMode (37, OUTPUT);pinMode(7,OUTPUT); // new       - alt TILT_ROLL
+  // GBIN : this is redefined for the AIO Pro below
+  //#define SERVO_1_PINMODE            pinMode(34,OUTPUT);pinMode(44,OUTPUT); // TILT_PITCH - WING left
+  //#define SERVO_1_PIN_HIGH           PORTC |= 1<<3;PORTL |= 1<<5;
+  //#define SERVO_1_PIN_LOW            PORTC &= ~(1<<3);PORTL &= ~(1<<5);
+  //#define SERVO_2_PINMODE            pinMode(35,OUTPUT);pinMode(45,OUTPUT); // TILT_ROLL  - WING right
+  //#define SERVO_2_PIN_HIGH           PORTC |= 1<<2;PORTL |= 1<<4;
+  //#define SERVO_2_PIN_LOW            PORTC &= ~(1<<2);PORTL &= ~(1<<4);
+  //#define SERVO_3_PINMODE            pinMode(33,OUTPUT); pinMode(46,OUTPUT); // CAM TRIG  - alt TILT_PITCH
+  //#define SERVO_3_PIN_HIGH           PORTC |= 1<<4;PORTL |= 1<<3;
+  //#define SERVO_3_PIN_LOW            PORTC &= ~(1<<4);PORTL &= ~(1<<3);
+  //#define SERVO_4_PINMODE            pinMode (37, OUTPUT);pinMode(7,OUTPUT); // new       - alt TILT_ROLL
   #define SERVO_4_PIN_HIGH           PORTC |= 1<<0; PORTH |= 1<<4;
   #define SERVO_4_PIN_LOW            PORTC &= ~(1<<0);PORTH &= ~(1<<4);
 
